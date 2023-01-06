10c3da339b7e974cbc21a9120fa76f1521072720
vmxdev <vm@xenoeye.com>
2023-01-06 22:09:25 +0300

    Add date and time display in telegram bot

--- scripts/telegram-bot/on-stop.sh
+++ scripts/telegram-bot/on-stop.sh
@@ -8,6 +8,8 @@ filename_src=${args[3]}
 filename_dst=${msgs_dir}${filename_src##*/}".s"
 filename_dst_tmp=${filename_dst}".stmp"
 
-echo -n "Overlimit is over, object '<b>${args[0]}</b>', IP <b>${args[4]}</b>, proto <b>${args[5]}</b>, BPS <b>$((${args[6]}*8))</b>, limit $((${args[7]}*8)) BPS" > ${filename_dst_tmp}
+dt=$(date '+%F %T.%3N');
+
+echo -n "Overlimit is over at '<b>${dt}</b>', object '<b>${args[0]}</b>', IP <b>${args[4]}</b>, proto <b>${args[5]}</b>, BPS <b>$((${args[6]}*8))</b>, limit $((${args[7]}*8)) BPS" > ${filename_dst_tmp}
 mv ${filename_dst_tmp} ${filename_dst}
 echo "BACK-"${filename_dst}
