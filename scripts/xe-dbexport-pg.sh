dc6d210f15abd961e289567477ecbc9e0a3cabcc
vmxdev <vm@xenoeye.com>
2025-02-06 15:09:38 +0300

    Add DB export script

---
+++ scripts/xe-dbexport-pg.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+EXP_DIR="/var/lib/xenoeye/exp"
+FAIL_DIR="/var/lib/xenoeye/expfailed/"
+
+FILES=`ls $EXP_DIR/*.sql`
+if [ -z "$FILES" ]; then
+    echo "No files"
+    exit
+fi
+
+TMPFILE=$(mktemp $EXP_DIR/tmp.XXXXXX)
+
+{ echo "BEGIN;" ; cat $FILES; echo "COMMIT;"; } > "$TMPFILE"
+psql postgresql://xenoeye:password@localhost/xenoeyedb -f "$TMPFILE" > /dev/null 2>/dev/null
+
+if [ $? -eq 0 ]; then
+    rm -f $FILES
+else
+    mv $FILES $FAIL_DIR
+fi
+rm -r "$TMPFILE"
